e90ed6eab4e0076932e9b05474339138dfc32a3f
Charles Schleich <charles@thoughtexpress.com>
2020-10-06 23:18:18 +0200

    146, accepted solution, but slow, 740ms

--- 146_lru-cache/include/mod1/LRUCache.hpp
+++ 146_lru-cache/include/mod1/LRUCache.hpp
@@ -18,7 +18,7 @@ class LRUCache {
 private:
     int capacity{0};
     std::list<std::pair <int, int>> dll;
-    // std::unordered_map<int, std::list<std::pair <int, int>>::iterator> hashmap;
+    std::unordered_map<int, std::list<std::pair <int, int>>::iterator> hashmap;
 
     bool initialized{false};
 public:
@@ -29,25 +29,17 @@ public:
     int get(int key) {
         using namespace std;
 
-        list<pair <int, int>>::iterator it = dll.begin();
-        bool found = false;
-        int v;
-
-        while(it != dll.end() ){
-            if(it->first == key){
-                found = true;
-                v = it->second;
-                break;
-            }
-            it++;
-        }
-
-        if (found == false){
+        if (hashmap.count(key)==0){
             return -1;
         } else {
             // re-order to get found value to the front.
-            dll.remove(make_pair(key,v));
-            dll.push_front(make_pair(key,v));
+            // hashmap.count(key);
+            list<pair <int, int>>::iterator iter = hashmap[key];
+            int v = iter->second;
+            pair<int,int> p = make_pair(key,v);
+            dll.remove(p);
+            dll.push_front(p);
+            hashmap[key] = dll.begin();
             return v;
         }
     }
@@ -59,13 +51,16 @@ public:
             // doesnt exist 
             if (dll.size() == capacity){
                 // Check size and pop if full
+                list<pair <int, int>>::iterator iter = dll.end();
+                --iter;
+                hashmap.erase(iter->first);
                 dll.pop_back();
             };
             // add to front of list
             dll.emplace_front(make_pair(key,value));
+            hashmap[key] = dll.begin(); 
         } else {
-            dll.pop_front();
-            dll.emplace_front(make_pair(key,value));
+            dll.begin()->second = value;
         }
     };
 
@@ -73,23 +68,25 @@ public:
         using namespace std;
 
         list<pair <int, int>>::iterator it = dll.begin();
-
-        if (!initialized)
-            return;
-        int k=0,v=0;
-        // cout<< dll.end();
+        if (!initialized) return;
         cout<< "    Mem("<< dll.size() <<"): ";
-
         while(true)
         {
-            if (it == dll.end()){
-                break;
-            } 
+            if (it == dll.end()) break; 
             cout<< " (" <<  it->first << ","<< it->second <<") ";
             it++;
         }
         cout << endl;
+
+        // Print out values in Hash map 
+        cout<< "    Hash  :(";
+        for( unordered_map<int, list<pair <int, int>>::iterator>::const_iterator it = hashmap.begin(); it != hashmap.end(); ++it){
+            std::cout << it->first << ", ";
+        }
+        cout<< ")" << "\n";
+
     }
+
 };
 
 
