3e76d209d9c9dddc39e1899e5d33d4d72c41d71e
Charles Schleich <schleich.cdaniel@gmail.com>
2020-10-08 12:20:17 +0200

    1 : massive speedup using hashmap

--- 1_two_sum/main.cpp
+++ 1_two_sum/main.cpp
@@ -15,13 +15,15 @@ using namespace std;
 void run_test(vector<int>& v, int target){
 	Solution *s1 = new Solution{};
 	vector<int> ans = s1->twoSum(v, target);
+	cout<< "(";
 	for (int n : ans){
-		cout<< n << endl;
+		cout<< n << ",";
 	}
+	cout << ")" << endl;
 }
 
 void test1(){
-	vector<int> v{ 3,2,4}; 
+	vector<int> v{3,2,4}; 
 	int target = 6;
 	run_test(v,target);
 }
@@ -32,10 +34,26 @@ void test2(){
 	run_test(v,target);
 }
 
+void dev(){
+	vector<int> v{ 2,7,11,15}; 
+	unordered_map<int, int> hmap;
 
+	hmap[v[0]] = 0; 
+	hmap[v[1]] = 1; 
+	hmap[v[2]] = 2; 
+	hmap[v[3]] = 3;
+
+	cout << hmap[v[3]] << endl;
+	cout << "-" << (hmap.find(14) == hmap.end()) << "-" << endl;
+	cout << "-" << (hmap.find(11) == hmap.end()) << "-" << endl;
+	cout << "-" << hmap[1254] << "-" << endl;
+
+}
 
 int main(int argc, char const *argv[])
 {
 	test1();
+	test2();
+	// dev();
 	return 0;
 } 
--- 1_two_sum/two_sum.cpp
+++ 1_two_sum/two_sum.cpp
@@ -1,51 +1,46 @@
-#include <iostream>
 #include <vector> 
+#include <unordered_map> 
 
 using namespace std; 
 
 class Solution {
 public:
+	// // HASH MAP SOLUTION
  	vector<int> twoSum(vector<int>& nums, int target) {
-	   	int size = nums.size();
+	   	unordered_map<int, int> hmap;
+		int size = nums.size();
+
        	for (int i = 0; i<size;i++){
-			for (int j = i+1; j<size;j++){
-				if (nums[i] +nums[j] == target){
-                    vector<int> ans{i,j};
-					return ans;
-				}
+			hmap[nums[i]] = i;
+		}
+
+       	for (int i = 0; i<size;i++){
+			if (hmap.find(target-nums[i]) != hmap.end()) {
+				if(hmap[target-nums[i]]==i){continue;};
+				vector<int> ans{i, hmap[target-nums[i]]};
+       			return ans;
 			}
 		}
+
        vector<int> ans{};
        return ans;
     }
+
+
+	// // BRUTE FORCE SOLUTION
+ 	// vector<int> twoSum(vector<int>& nums, int target) {
+	//    	int size = nums.size();
+    //    	for (int i = 0; i<size;i++){
+	// 		for (int j = i+1; j<size;j++){
+	// 			if (nums[i] +nums[j] == target){
+    //                 vector<int> ans{i,j};
+	// 				return ans;
+	// 			}
+	// 		}
+	// 	}
+    //    vector<int> ans{};
+    //    return ans;
+    // }
+
 };
 
-
-// Sad that Iterators are too slow 
-
-// vector<int> twoSum(vector<int>& nums, int target) {
-// 	cout<<"twoSum"<< endl;
-
-// 	vector<int> ans{};		
-// 	vector<int>::iterator it = nums.begin();
-
-// 	while (it != nums.end()){
-// 		if (*it>=target){
-// 			++it;
-// 			continue;
-// 		}
-// 		vector<int>::iterator it2 = it;
-// 		while (it2 != nums.end()){
-// 			if (*it2>=target) continue;
-// 			if (*it2+*it == target){
-// 				ans.push_back(*it);
-// 				ans.push_back(*it2);
-// 				return ans;
-// 			}
-// 			++it2;
-// 		}		
-// 		++it;
-// 	}
-
-// 	return ans;
-// }
